7e68e070a593d78711f2ea19b7facd03c8a11dc6
Anthony <anthonyjquattrocchi@gmail.com>
2018-07-20 10:45:41 -0700

    Files created. HTML structure created. Stubbed plan.

---
+++ app.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+console.log('+++ filename: app.js: jquery.ready exoected no error actual: ', $ );
+
+});
---
+++ plan.js
@@ -0,0 +1,53 @@
+//Goals
+//crud
+
+//accept data -- form, event listener, function to accept to form data
+
+//store data -- accept validated form data to write to local storage
+
+//retrieve data -- button, function to grab storage, function to update display
+
+//update data -- leave this to me
+
+//delete data-- delete function
+
+
+MVP 
+// a form with 2 fields, 2 buttons(one to get, one to set), working local storage
+	//able to get and set data in local storage via the form input
+
+
+
+//1. create a form 	√
+
+
+//2. add event listener(s)
+
+//3. Learn about local storage
+
+//4. implement basic write to local storage (hard code)
+
+//5. implement basic read from local storage
+
+//6. make read/write dynamic (attached to form events)
+//6a. update data
+
+//7. CSS
+
+//8. UX -- input confirmation
+
+
+
+MVP+ 
+//above and beyond
+
+//filter bar that searches through tag/code and returns functions that fit categories
+
+//return message that confirms the function has been stored (toastr message)
+
+//manage local storage data based on purpose/intent
+
+//rebuild assistant
+//different users
+
+
