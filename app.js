76fefbd93e1489d04a7e04875c8f753946296731
Anthony <anthonyjquattrocchi@gmail.com>
2018-07-25 15:19:16 -0700

    Update, remove, delete added. Editor synched. CSS finished.

--- app.js
+++ app.js
@@ -1,30 +1,59 @@
 $(document).ready(function() {
-console.log('+++ filename: app.js: jquery.ready exoected no error actual: ', $ );
-
-//attach event listener to buttons(input)
-//create function stub for read/write/delete
 $('.store-button').on('click', function(event) {
-	// event.preventDefault();
-	// event.stopPropagation(); //stops event from leaking into other listeners
-	// console.log(event.target.classList);
 	let titleValue = $('.input-title-field').val();
-    let codeValue = $('.input-code-field').val();
+  let titleBox = $(`<div class="snippet-title">${titleValue}</div>`).delay(120000).fadeOut(3000);
+	if (titleValue === "") {
+    alert("Enter a valid file name");
+    return false;
+  }
+  localStorage.setItem(`${titleValue}`, editor.getValue());
+    titleBox.appendTo('.snippet-box');
+});
 
-	localStorage.setItem('titleValue', titleValue);
-	localStorage.setItem('codeValue', codeValue);
-
-    
+$('.snippet-box').on('click', function() {
+   let snippetTitle = $('.snippet-title').html();
+   console.log(snippetTitle);
+   let titleValue = $('.input-title-field').val();
+   for (var x = 0; x < localStorage.length; x++) {
+    if (snippetTitle === localStorage.key(x)) {
+      var newCode = localStorage.getItem(`${localStorage.key(x)}`);
+        editor.setValue(newCode);
+      }
+    }
 });
 
 $('.get-button').on('click', function(event) {
-	// localStorage.getItem('booya');
-	let titleValue = localStorage.getItem('titleValue');
-	let codeValue = localStorage.getItem('codeValue'); //booya is the property
-	$('.debug').html(`<p>${titleValue} ${codeValue}</p>`);
+  let titleValue = $('.input-title-field').val();
+  let titleBox = $(`<div class="snippet-title">${titleValue}</div>`).delay(120000).fadeOut(10000);
+
+    for (var x = 0; x < localStorage.length; x++) {
+    	if (titleValue === localStorage.key(x)) {
+    		var newCode = localStorage.getItem(`${localStorage.key(x)}`);
+    		editor.setValue(newCode);
+    	} 
+    }
+    titleBox.appendTo('.snippet-box'); 
 });
 
 $('.delete-button').on('click', function(event) {
-	console.log(localStorage.removeItem('booya')); //deletes given property
+	let titleValue = $('.input-title-field').val();
+  let snippetTitle = $('.snippet-box').html();
+  let snippetsDiv = $('.snippet-box');
+    let result = confirm("Are you sure you want to delete this code?");
+    if (result) {
+    for (var x = 0; x < localStorage.length; x++) {
+      if (titleValue === localStorage.key(x)) {
+        localStorage.removeItem(localStorage.key(x));
+      } 
+    }
+  }
 });
 
+$('.run-button').on('click', function(event) {
+  let code = editor.getValue();
+  let titleValue = $('.input-title-field').val();
+  let titleBox = $(`<div class="snippet-title">${titleValue}</div>`).delay(120000).fadeOut(3000);
+  titleBox.appendTo('.snippet-box');
+  console.log(eval(code));
+});
 });
